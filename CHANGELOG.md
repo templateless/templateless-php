fcf32921f40e3d9ba894e22c09a252bc916c4ab3
Ivan Akimov <ivan@templateless.com>
2024-02-16 11:00:18 -0600

    chore: new socials services

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,7 +1,19 @@
 # CHANGELOG
 
 **Unpublished:**
-- Introduced CHANGELOG
+- Introduced `CHANGELOG.md`
+- Introduced new services as social icons:
+  - `Service::PHONE` (converts into a link with `tel:` prefix)
+  - `Service::FACEBOOK`
+  - `Service::YOUTUBE`
+  - `Service::INSTAGRAM`
+  - `Service::LINKEDIN`
+  - `Service::SLACK`
+  - `Service::DISCORD`
+  - `Service::TIKTOK`
+  - `Service::SNAPCHAT`
+  - `Service::THREADS`
+  - `Service::TELEGRAM`
 
 **v0.1.0-alpha.0:**
 - Initial implementation
--- src/Components/Socials.php
+++ src/Components/Socials.php
@@ -11,9 +11,20 @@ enum Service: string
 {
     case WEBSITE = 'WEBSITE';
     case EMAIL = 'EMAIL';
+    case PHONE = 'PHONE';
+    case FACEBOOK = 'FACEBOOK';
+    case YOUTUBE = 'YOUTUBE';
     case TWITTER = 'TWITTER';
     case X = 'X';
     case GITHUB = 'GITHUB';
+    case INSTAGRAM = 'INSTAGRAM';
+    case LINKEDIN = 'LINKEDIN';
+    case SLACK = 'SLACK';
+    case DISCORD = 'DISCORD';
+    case TIKTOK = 'TIKTOK';
+    case SNAPCHAT = 'SNAPCHAT';
+    case THREADS = 'THREADS';
+    case TELEGRAM = 'TELEGRAM';
 }
 
 class Item implements JsonSerializable
